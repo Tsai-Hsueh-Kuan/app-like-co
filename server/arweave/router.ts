7076f7cb48ad84a7ba6ea8628214e00e8b3ee9bb
hsueh kuan tsai <b123456785678@gmail.com>
2022-08-11 12:38:35 +0800

    🚧 Console

--- server/arweave/router.ts
+++ server/arweave/router.ts
@@ -48,7 +48,9 @@ router.post('/upload',
   try {
     const shouldRegisterNUMAssets = req.body.num && req.body.num !== '0';
     const files = req.files as Express.Multer.File[];
+    console.log('point 0')
     const arFiles = convertMulterFiles(files);
+    console.log('point 1')
     const [
       ipfsHash,
       prices,
@@ -56,6 +58,7 @@ router.post('/upload',
       getIPFSHash(arFiles),
       estimateARPrices(arFiles),
     ]);
+    console.log('point 2')
     const { arweaveId: existingArweaveId } = prices;
 
     // shortcut for existing file without checking tx
@@ -74,7 +77,7 @@ router.post('/upload',
       })
       return;
     }
-
+    console.log('point 3')
     const { txHash } = req.query;
     if (!txHash) {
       res.status(400).send('MISSING_TX_HASH');
@@ -89,38 +92,59 @@ router.post('/upload',
       if (!tx && tryTimes < ARWEAVE_UPLOAD_TRY_LIMIT) await timeout(2000);
       /* eslint-enable no-await-in-loop */
     } while (!tx && tryTimes < ARWEAVE_UPLOAD_TRY_LIMIT)
+    console.log('point 4')
 
     if (!tx || !tx?.amount) {
       res.status(400).send('TX_NOT_FOUND');
       return;
     }
+    console.log('point 5')
     const { memo, amount } = tx;
     let memoIPFS = '';
     try {
       ({ ipfs: memoIPFS } = JSON.parse(memo));
     } catch (err) {
       // ignore non-JSON memo
+    console.log('point 6')
     }
+    console.log('point 7')
+
     if (!memoIPFS || memoIPFS !== ipfsHash) {
+    console.log('point 8')
+
       res.status(400).send('TX_MEMO_NOT_MATCH');
       return;
     }
+    console.log('point 9')
+
     const { LIKE } = await convertARPricesToLIKE(prices, { margin: 0.03 });
+    console.log('point 10')
+
     const txAmount = new BigNumber(amount.amount).shiftedBy(-9);
+    console.log('point 11')
+
     if (txAmount.lt(LIKE)) {
+    console.log('point 12')
+
       res.status(400).send('TX_AMOUNT_NOT_ENOUGH');
       return;
     }
+    console.log('point 13')
+
     const promises: Promise<any>[] = [
       uploadFilesToArweave(arFiles),
       uploadFilesToIPFS(arFiles),
     ];
+    console.log('point 14')
+
     if (shouldRegisterNUMAssets) {
       promises.push(registerNUMAssets(arFiles.map(file => ({
         file: file.buffer,
         filename: file.filename,
       }))))
     }
+    console.log('point 15')
+
     const [
       {
         arweaveId,
@@ -130,6 +154,8 @@ router.post('/upload',
       _,
       numAssetIds,
     ] = await Promise.all(promises)
+    console.log('point 16')
+
     res.json({ arweaveId, ipfsHash, list, numAssetIds: numAssetIds || [] });
   } catch (error) {
     next(error);
